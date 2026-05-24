ba573dcb76beeaeaa5391823611f398a246b8cd0
jaredotero03 <jaredotero03@gmail.com>
2026-05-23 20:04:20 -0600

    Login_view update v1

--- Inclusivejob_IA/src/App.jsx
+++ Inclusivejob_IA/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from "./pages/Home.jsx";
+import Login from './login/login.jsx';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </div>
   );
